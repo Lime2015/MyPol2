2548f1093017c3c6572defcd8f7d337f548a3cd3
seongsan <eekek@naver.com>
2015-08-16 14:19:09 +0900

    fff

---
+++ mypol/src/main/java/com/lime/mypol/fragment/MenusFragment.java
@@ -0,0 +1,30 @@
+package com.lime.mypol.fragment;
+
+import com.lime.mypol.R;
+
+/**
+ * Created by Gordon Wong on 7/17/2015.
+ *
+ * Favorite items fragment.
+ */
+public class MenusFragment extends NotesListFragment {
+
+	public static MenusFragment newInstance() {
+		return new MenusFragment();
+	}
+
+	@Override
+	protected int getLayoutResId() {
+		return R.layout.fragment_menus;
+	}
+
+	@Override
+	protected int getNumColumns() {
+		return 1;
+	}
+
+	@Override
+	protected int getNumItems() {
+		return 7;
+	}
+}
